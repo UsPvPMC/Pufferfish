fdbeda6da31a98a99316d586a851dceb3ed17eac
Aikar <aikar@aikar.co>
2017-01-07 15:24:46 -0500

    Provide E/TE/Chunk count stat methods
    Provides counts without the ineffeciency of using .getEntities().size()
    which creates copy of the collections.

--- src/main/java/net/minecraft/world/level/Level.java
+++ src/main/java/net/minecraft/world/level/Level.java
@@ -117,7 +117,7 @@ public abstract class Level implements LevelAccessor, AutoCloseable {
     public static final int TICKS_PER_DAY = 24000;
     public static final int MAX_ENTITY_SPAWN_Y = 20000000;
     public static final int MIN_ENTITY_SPAWN_Y = -20000000;
-    protected final List<TickingBlockEntity> blockEntityTickers = Lists.newArrayList();
+    protected final List<TickingBlockEntity> blockEntityTickers = Lists.newArrayList(); public final int getTotalTileEntityTickers() { return this.blockEntityTickers.size(); } // Paper
     protected final NeighborUpdater neighborUpdater;
     private final List<TickingBlockEntity> pendingBlockEntityTickers = Lists.newArrayList();
     private boolean tickingBlockEntities;
