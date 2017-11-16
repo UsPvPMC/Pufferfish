770d34be5f595deb8fb8d482ad1e1cf249aa5201
Shane Freeder <theboyetronic@gmail.com>
2017-11-16 12:12:41 +0000

    use CB BlockState implementations for captured blocks
    When modifying the world, CB will store a copy of the affected
    blocks in order to restore their state in the case that the event
    is cancelled. This change only modifies the collection of blocks
    in the world by normal means, e.g. not during tree population,
    as the potentially marginal overheads would serve no advantage.
    
    CB was using a CraftBlockState for all blocks, which causes issues
    should any block that uses information beyond a data ID would suffer
    from missing information, e.g. Skulls.
    
    By using CBs CraftBlock#getState(), we will maintain a proper copy of
    the blockstate that will be valid for restoration, as opposed to dropping
    information on restoration when the event is cancelled.

--- src/main/java/net/minecraft/world/level/Level.java
+++ src/main/java/net/minecraft/world/level/Level.java
@@ -155,7 +155,7 @@ public abstract class Level implements LevelAccessor, AutoCloseable {
     public boolean preventPoiUpdated = false; // CraftBukkit - SPIGOT-5710
     public boolean captureBlockStates = false;
     public boolean captureTreeGeneration = false;
-    public Map<BlockPos, CapturedBlockState> capturedBlockStates = new java.util.LinkedHashMap<>();
+    public Map<BlockPos, org.bukkit.craftbukkit.block.CraftBlockState> capturedBlockStates = new java.util.LinkedHashMap<>(); // Paper
     public Map<BlockPos, BlockEntity> capturedTileEntities = new HashMap<>();
     public List<ItemEntity> captureDrops;
     public final it.unimi.dsi.fastutil.objects.Object2LongOpenHashMap<SpawnCategory> ticksPerSpawnCategory = new it.unimi.dsi.fastutil.objects.Object2LongOpenHashMap<>();
@@ -386,7 +386,7 @@ public abstract class Level implements LevelAccessor, AutoCloseable {
     public boolean setBlock(BlockPos pos, BlockState state, int flags, int maxUpdateDepth) {
         // CraftBukkit start - tree generation
         if (this.captureTreeGeneration) {
-            CapturedBlockState blockstate = this.capturedBlockStates.get(pos);
+            CraftBlockState blockstate = this.capturedBlockStates.get(pos);
             if (blockstate == null) {
                 blockstate = CapturedBlockState.getTreeBlockState(this, pos, flags);
                 this.capturedBlockStates.put(pos.immutable(), blockstate);
@@ -406,7 +406,8 @@ public abstract class Level implements LevelAccessor, AutoCloseable {
             // CraftBukkit start - capture blockstates
             boolean captured = false;
             if (this.captureBlockStates && !this.capturedBlockStates.containsKey(pos)) {
-                CapturedBlockState blockstate = CapturedBlockState.getBlockState(this, pos, flags);
+                CraftBlockState blockstate = (CraftBlockState) world.getBlockAt(pos.getX(), pos.getY(), pos.getZ()).getState(); // Paper - use CB getState to get a suitable snapshot
+                blockstate.setFlag(flags); // Paper - set flag
                 this.capturedBlockStates.put(pos.immutable(), blockstate);
                 captured = true;
             }
@@ -618,7 +619,7 @@ public abstract class Level implements LevelAccessor, AutoCloseable {
     public BlockState getBlockState(BlockPos pos) {
         // CraftBukkit start - tree generation
         if (this.captureTreeGeneration) {
-            CapturedBlockState previous = this.capturedBlockStates.get(pos);
+            CraftBlockState previous = this.capturedBlockStates.get(pos); // Paper
             if (previous != null) {
                 return previous.getHandle();
             }
