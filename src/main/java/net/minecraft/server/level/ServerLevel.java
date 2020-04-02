b7292c1791a0abb3797d7c9e8d4ed9c38b6fd163
Aikar <aikar@aikar.co>
2020-04-02 01:42:39 -0400

    Prevent Double PlayerChunkMap adds crashing server
    Suspected case would be around the technique used in .stopRiding
    Stack will identify any causer of this and warn instead of crashing.

--- src/main/java/net/minecraft/server/level/ServerLevel.java
+++ src/main/java/net/minecraft/server/level/ServerLevel.java
@@ -2324,7 +2324,7 @@ public class ServerLevel extends Level implements WorldGenLevel {
 
         public void onTrackingStart(Entity entity) {
             org.spigotmc.AsyncCatcher.catchOp("entity register"); // Spigot
-            ServerLevel.this.getChunkSource().addEntity(entity);
+            // ServerLevel.this.getChunkSource().addEntity(entity); // Paper - moved down below valid=true
             if (entity instanceof ServerPlayer) {
                 ServerPlayer entityplayer = (ServerPlayer) entity;
 
@@ -2358,6 +2358,7 @@ public class ServerLevel extends Level implements WorldGenLevel {
 
             entity.updateDynamicGameEventListener(DynamicGameEventListener::add);
             entity.valid = true; // CraftBukkit
+            ServerLevel.this.getChunkSource().addEntity(entity);
             // Paper start - Set origin location when the entity is being added to the world
             if (entity.getOriginVector() == null) {
                 entity.setOrigin(entity.getBukkitEntity().getLocation());
