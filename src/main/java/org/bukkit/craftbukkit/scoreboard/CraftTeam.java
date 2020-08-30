9ab275a91b4aa4a5593418b437205cb298c57ae4
foss-mc <69294560+foss-mc@users.noreply.github.com>
2020-08-30 15:30:29 +0800

    Fix CraftTeam null check

--- src/main/java/org/bukkit/craftbukkit/scoreboard/CraftTeam.java
+++ src/main/java/org/bukkit/craftbukkit/scoreboard/CraftTeam.java
@@ -261,7 +261,7 @@ final class CraftTeam extends CraftScoreboardComponent implements Team {
 
     @Override
     public boolean hasEntry(String entry) throws IllegalArgumentException, IllegalStateException {
-        Validate.notNull("Entry cannot be null");
+        Validate.notNull(entry, "Entry cannot be null"); // Paper
 
         CraftScoreboard scoreboard = this.checkState();
 
