fcbd53016d95ace09903929f705100f1cbbe4ede
Yive <admin@yive.me>
2021-01-24 08:55:19 -0800

    Reset shield blocking on dimension change

--- src/main/java/net/minecraft/server/level/ServerPlayer.java
+++ src/main/java/net/minecraft/server/level/ServerPlayer.java
@@ -1198,6 +1198,11 @@ public class ServerPlayer extends Player {
                 this.level.getCraftServer().getPluginManager().callEvent(changeEvent);
                 // CraftBukkit end
             }
+            // Paper start
+            if (this.isBlocking()) {
+                this.stopUsingItem();
+            }
+            // Paper end
 
             return this;
         }
