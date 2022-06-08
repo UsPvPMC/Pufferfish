3cf982dd2f7238c33cf0930904e6e712dcb5784e
Nassim Jahnke <nassim@njahnke.dev>
2022-06-08 10:52:18 +0200

    Move range check for block placing up

--- src/main/java/net/minecraft/server/network/ServerGamePacketListenerImpl.java
+++ src/main/java/net/minecraft/server/network/ServerGamePacketListenerImpl.java
@@ -1812,6 +1812,11 @@ public class ServerGamePacketListenerImpl implements ServerPlayerConnection, Tic
         if (itemstack.isItemEnabled(worldserver.enabledFeatures())) {
             BlockHitResult movingobjectpositionblock = packet.getHitResult();
             Vec3 vec3d = movingobjectpositionblock.getLocation();
+            // Paper start - improve distance check
+            if (!Double.isFinite(vec3d.x) || !Double.isFinite(vec3d.y) || !Double.isFinite(vec3d.z)) {
+                return;
+            }
+            // Paper end
             BlockPos blockposition = movingobjectpositionblock.getBlockPos();
             Vec3 vec3d1 = Vec3.atCenterOf(blockposition);
 
