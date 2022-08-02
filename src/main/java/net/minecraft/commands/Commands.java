e108ae28f3b51178f3e0000dc04a09a204e986bd
chickeneer <emcchickeneer@gmail.com>
2022-08-01 20:13:02 -0500

    Fix suggest command message for brigadier syntax exceptions
    This is a bug accidentally introduced in upstream CB

--- src/main/java/net/minecraft/commands/Commands.java
+++ src/main/java/net/minecraft/commands/Commands.java
@@ -340,7 +340,7 @@ public class Commands {
                 if (commandsyntaxexception.getInput() != null && commandsyntaxexception.getCursor() >= 0) {
                     int j = Math.min(commandsyntaxexception.getInput().length(), commandsyntaxexception.getCursor());
                     MutableComponent ichatmutablecomponent = Component.empty().withStyle(ChatFormatting.GRAY).withStyle((chatmodifier) -> {
-                        return chatmodifier.withClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, label)); // CraftBukkit
+                        return chatmodifier.withClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/" + label)); // CraftBukkit // Paper
                     });
 
                     if (j > 10) {
