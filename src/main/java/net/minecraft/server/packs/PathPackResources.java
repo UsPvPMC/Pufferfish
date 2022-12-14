4b236b7bcb968c6862bbfddf4d0835b6a256cd84
Jake Potrebic <jake.m.potrebic@gmail.com>
2022-12-14 15:52:11 -0800

    Improve logging and errors

--- src/main/java/net/minecraft/server/packs/PathPackResources.java
+++ src/main/java/net/minecraft/server/packs/PathPackResources.java
@@ -105,6 +105,12 @@ public class PathPackResources extends AbstractPackResources {
         try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)) {
             for(Path path2 : directoryStream) {
                 String string = path2.getFileName().toString();
+                // Paper start
+                if (!Files.isDirectory(path2)) {
+                    LOGGER.error("Invalid directory entry: {} in {}.", string, this.root, new java.nio.file.NotDirectoryException(string));
+                    continue;
+                }
+                // Paper end
                 if (string.equals(string.toLowerCase(Locale.ROOT))) {
                     set.add(string);
                 } else {
