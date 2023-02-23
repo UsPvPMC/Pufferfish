b9b4a84ec3d28d9584a33bf80f52a637c8d76759
Jake Potrebic <jake.m.potrebic@gmail.com>
2023-02-23 13:19:13 -0800

    Fix SpawnEggMeta#get/setSpawnedType

--- src/main/java/org/bukkit/craftbukkit/inventory/CraftMetaSpawnEgg.java
+++ src/main/java/org/bukkit/craftbukkit/inventory/CraftMetaSpawnEgg.java
@@ -216,6 +216,31 @@ public class CraftMetaSpawnEgg extends CraftMetaItem implements SpawnEggMeta {
         throw new UnsupportedOperationException("Must change item type to set spawned type");
     }
 
+    // Paper start
+    @Override
+    public EntityType getCustomSpawnedType() {
+        return java.util.Optional.ofNullable(this.entityTag)
+            .map(tag -> tag.getString(ENTITY_ID.NBT))
+            .flatMap(net.minecraft.world.entity.EntityType::byString)
+            .map(org.bukkit.craftbukkit.util.CraftMagicNumbers::getEntityType)
+            .orElse(null);
+    }
+
+    @Override
+    public void setCustomSpawnedType(final EntityType type) {
+        if (type == null) {
+            if (this.entityTag != null) {
+                this.entityTag.remove(ENTITY_ID.NBT);
+            }
+        } else {
+            if (this.entityTag == null) {
+                this.entityTag = new CompoundTag();
+            }
+            this.entityTag.putString(ENTITY_ID.NBT, type.key().toString());
+        }
+    }
+    // Paper end
+
     @Override
     boolean equalsCommon(CraftMetaItem meta) {
         if (!super.equalsCommon(meta)) {
