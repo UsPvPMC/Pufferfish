Akarin-project/Akarin

Add creative mode NBT permissions

src/main/java/net/minecraft/server/PlayerConnection.java
@@ -2246,7 +2246,7 @@ public class PlayerConnection implements PacketListenerPlayIn, ITickable {
             ItemStack itemstack = packetplayinsetcreativeslot.getItemStack();
             NBTTagCompound nbttagcompound = itemstack.b("BlockEntityTag");
 
-            if (!itemstack.isEmpty() && nbttagcompound != null && nbttagcompound.hasKey("x") && nbttagcompound.hasKey("y") && nbttagcompound.hasKey("z")) {
+            if (!itemstack.isEmpty() && nbttagcompound != null && nbttagcompound.hasKey("x") && nbttagcompound.hasKey("y") && nbttagcompound.hasKey("z") && this.player.getBukkitEntity().hasPermission("minecraft.nbt.copy")) { // Spigot
                 BlockPosition blockposition = new BlockPosition(nbttagcompound.getInt("x"), nbttagcompound.getInt("y"), nbttagcompound.getInt("z"));
                 TileEntity tileentity = this.player.world.getTileEntity(blockposition);
 

UsPvPMC/Pufferfish

src/main/java/net/minecraft/server/network/PlayerConnection.java
@@ -2905,7 +2905,7 @@ public class PlayerConnection implements ServerPlayerConnection, TickablePacketL
 
             NBTTagCompound nbttagcompound = ItemBlock.getBlockEntityData(itemstack);
 
-            if (!itemstack.isEmpty() && nbttagcompound != null && nbttagcompound.contains("x") && nbttagcompound.contains("y") && nbttagcompound.contains("z")) {
+            if (!itemstack.isEmpty() && nbttagcompound != null && nbttagcompound.contains("x") && nbttagcompound.contains("y") && nbttagcompound.contains("z") && this.player.getBukkitEntity().hasPermission("minecraft.nbt.copy")) { // Spigot
                 BlockPosition blockposition = TileEntity.getPosFromTag(nbttagcompound);
 
                 if (this.player.level.isLoaded(blockposition)) {